d56a2ddc775199d7c8dd22da5a34cacb12f3656e
Juliano Nascimento <julianonascimento.ti@gmail.com>
2020-04-29 21:11:49 -0300

    method Sum

---
+++ RestWebAPI/RestWebAPI/Controllers/CalculatorController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace RestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if(IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string Number)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(Number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        private bool IsNumeric(string pNumber)
+        {
+            double number;
+
+            bool isNumber = double.TryParse(pNumber, NumberStyles.Any, NumberFormatInfo.InvariantInfo, out number);
+
+            return isNumber;
+        }
+    }
+}
